b17c864a3dc6d716f1e716abbb86d45bfc0cc807
NexoTR <48154713+NexoTR@users.noreply.github.com>
2019-03-02 23:49:56 +0300

    Update sunucu.js

--- cmds/sunucu.js
+++ cmds/sunucu.js
@@ -1,15 +1,7 @@
 const Discord = require('discord.js')
 
 module.exports.run = async (bot, message, args) => {
-  let sunucu = new Discord.RichEmbed()
-  .setColor('#ff6523')
-  .setTitle(":grey_question:" + " " + "**" + `${message.guild.name}` + " " + "**Bilgileri**")
-  .setThumbnail(message.guild.iconURL())
-  .addField(":bust_in_silhouette: **Kurucu**",message.guild.owner.user.username + " " + "#"+ message.guild.owner.user.discriminator)
-  .addField(":calendar: **Kuruluş Tarihi**",message.guild.createdAt)
-  .addField(":id: **ID**",message.guild.id)
-  .addField(":regional_indicator_r: **Rol Sayısı**",message.guild.roles.size)
-  message.channel.send(sunucu)
+  message.channel.send(**:warning: Kullanılabilir Değil !**)
 }
 
 module.exports.help = {
