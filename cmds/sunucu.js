89959b4897f48dc6f0d8516ce4a69cb18dcf485f
NexoTR <48154713+NexoTR@users.noreply.github.com>
2019-03-03 00:55:44 +0300

    Update sunucu.js

--- cmds/sunucu.js
+++ cmds/sunucu.js
@@ -1,9 +1,11 @@
 const Discord = require('discord.js')
 
 module.exports.run = async (bot, message, args) => {
+	let ic = message.guild.iconURL;
 	let server = new Discord.RichEmbed()
 	.setColor('#ff6523')
-	.setTitle("**" + `${message.guild.name}` + "**" + " " + "#" + `${message.guild.id}` + " " + "Sunucu Bilgileri")
+	.setTitle("**" + `${message.guild.name}` + "**" + " " + "Sunucu Bilgileri")
+	.setThumbnail(ic)
 	message.channel.send(server)
 }
 
