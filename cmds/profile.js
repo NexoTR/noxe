a98e0f09e6a21066f701351f753a1abee64ce421
NexoTR <48154713+NexoTR@users.noreply.github.com>
2019-03-02 16:47:22 +0300

    Update profile.js

--- cmds/profile.js
+++ cmds/profile.js
@@ -3,26 +3,7 @@ const moment = require('moment');
 
 module.exports.run = async (bot, message, args) => {
 
-    let user;
-    if (message.mentions.user.first()) {
-        user = message.mentions.users.first();
-    } else {
-        user = message.author;
-    }
-    
-    const member = message.guild.member(user);
-    
-    const profile = new Discord.RichEmbed()
-    .setTitle("**" + `${user.username}` + "**" + " " + "#" + `${user.discriminator}`)
-    .setColor(`RANDOM`)
-    .setThumbnail(message.author.AvatarURL)
-    .addField("**Takma Adı**",``${member.nickname !== null ? `${member.nickname}` : : 'Yok')
-    .addField(":date: **Katılma Tarihi**",`${member.joinedAt}`)
-    .addField("**Bot**", `${user.bot}`)
-    .addField(":red_circle: **Durum**", `$user.presence.status}`)
-    .addField("**Oynuyor**",`${user.presence.game ? user.presence.game.name : 'Yok'}`)
-    .addField("**Rolleri**", `${member.roles.map(roles => `${roles.name}`).join(' | ')}`)c
-    message.channel.send({embed : profile});
+    message.channel.send('**:warning: Kullanılabilir Değil !**');
 }
 
 module.exports.help = {
