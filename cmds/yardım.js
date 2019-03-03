524f8213be53a5335c1569aafa5d345e8ebff9e0
NexoTR <48154713+NexoTR@users.noreply.github.com>
2019-03-04 00:20:01 +0300

    Update yardım.js

--- cmds/yardım.js
+++ cmds/yardım.js
@@ -2,13 +2,10 @@ const Discord = require('discord.js')
 
 module.exports.run = async (bot, message, args) => {
 	if(!message.member.hasPermission("ADMINISTRATOR")){
-		let help = new Discord.RichEmbed()
-		.setColor(`RANDOM`)
-		.setTitle("Komutlar")
-		.addField(```asciidoc
-			[Turuncu]
-			```,"test")
-		message.channel.send(help)
+		var num = args[0];
+		if (num.content === "eglence"){
+			message.channel.send("test")
+		}
 	}
 }
 
