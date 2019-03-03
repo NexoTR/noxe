b256b8021190a0be1945eba4130d8eab5f520ed5
NexoTR <48154713+NexoTR@users.noreply.github.com>
2019-03-03 13:35:13 +0300

    Update sunucu.js

--- cmds/sunucu.js
+++ cmds/sunucu.js
@@ -3,7 +3,6 @@ const moment = require('moment')
 
 module.exports.run = async (bot, message, args) => {
 	const createserver = moment(message.guild.createdAt).format('ll')
-	let person = guild.members.filter(member => !member.user.bot).size;
 	let ic = message.guild.iconURL;
 	let server = new Discord.RichEmbed()
 	.setColor('#ff6523')
@@ -12,7 +11,7 @@ module.exports.run = async (bot, message, args) => {
 	.addField(":calendar:**Kuruluş Tarihi**",createserver)
 	.addField(":satellite:**Kanal Sayısı**",message.guild.channels.size)
 	.addField(":regional_indicator_r: ** Rol Sayısı**",message.guild.roles.size)
-	.addField("**Kullanıcı Sayısı**", person)
+	.addField("**Kullanıcı Sayısı**", message.guild.members.filter(member => !member.user.bot === true).size)
 	.setFooter("ID = " + message.guild.id + " | " + "Bölge = " + message.guild.region)
 	message.channel.send(server)
 }
