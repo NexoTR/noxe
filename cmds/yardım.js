d08a427d583f9d2cd5f5af8f31a6b0ae95d9b3d0
NexoTR <48154713+NexoTR@users.noreply.github.com>
2019-03-04 01:14:02 +0300

    Add files via upload

--- cmds/yardım.js
+++ cmds/yardım.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js')
 module.exports.run = async (bot, message, args) => {
 	let help = new Discord.RichEmbed()
 	.setColor(`RANDOM`)
-	.addField("Kategoriler","• n!temel - Temel Komutlar \n • n!eglence - Eğlence Komutlar \n • n!yetki - Yetkili Komutlar")
+	.addField("Eğlence Komutları","• n!")
 	message.channel.send(help)
 }
 
