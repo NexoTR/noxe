3a3461ffcbac35d036796e7a2cd2da6232dbbec3
NexoTR <48154713+NexoTR@users.noreply.github.com>
2019-03-04 01:19:34 +0300

    Update yardım.js

--- cmds/yardım.js
+++ cmds/yardım.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js')
 module.exports.run = async (bot, message, args) => {
 	let help = new Discord.RichEmbed()
 	.setColor(`RANDOM`)
-	.addField("Eğlence Komutları","• n!")
+	.addField("Kategoriler","• n!temel - Temel Komutlar \n • n!eglence - Eğlence Komutlar \n • n!yetkili - Yetkili Komutlar")
 	message.channel.send(help)
 }
 
