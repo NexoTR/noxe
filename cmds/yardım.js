ca9bca6ee77baca0342ba04d4ce70cf3f7b27e1c
NexoTR <48154713+NexoTR@users.noreply.github.com>
2019-03-03 15:47:32 +0300

    Update yardım.js

--- cmds/yardım.js
+++ cmds/yardım.js
@@ -1,23 +1,14 @@
 const Discord = require('discord.js')
 
 module.exports.run = async (bot, message, args) => {
-	let first = args.join(" ")
 	if(!message.member.hasPermission("ADMINISTRATOR")){
 		let help = new Discord.RichEmbed()
-		.setColor('#ff6523')
-		.addField("Kategoriler","• n!yardım bot = Bot Bilgileri \n •n!yardım yetkili = Yetkili Komutları \n •n!yardım ayarlar = Bot Ayarları")
-		return message.member.send({ embed: help})
-	if(first === "yardım"){
-	message.channel.send("helo")
-	}
-	}
-	
-	
-	if(message.member.hasPermission("ADMINISTRATOR")){
-		let help = new Discord.RichEmbed()
-		.setColor('#ff6523')
-		.addField("Kategoriler","• n!yardım bot = Bot Bilgileri \n •n!yardım yetkili = Yetkili Komutları \n •n!yardım ayarlar = Bot Ayarları")
-	message.member.send({ embed: help})
+		.setColor(`RANDOM`)
+		.setTitle("Komutlar")
+		.addField(```asciidoc
+			[Turuncu]
+			```,"test")
+		message.channel.send(help)
 	}
 }
 
