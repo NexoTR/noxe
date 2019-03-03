0147c2793f205d02139d7a11b684298ba69eb8d7
NexoTR <48154713+NexoTR@users.noreply.github.com>
2019-03-03 12:30:22 +0300

    Update sunucu.js

--- cmds/sunucu.js
+++ cmds/sunucu.js
@@ -2,13 +2,15 @@ const Discord = require('discord.js')
 const moment = require('moment')
 
 module.exports.run = async (bot, message, args) => {
+	const createserver = moment(message.guild.createdAt).format('ll')
 	let ic = message.guild.iconURL;
 	let server = new Discord.RichEmbed()
 	.setColor('#ff6523')
 	.setTitle("**" + `${message.guild.name}` + "**" + " " + "Sunucu Bilgileri")
 	.setThumbnail(ic)
-	.addField(":calendar:**Kuruluş Tarihi**",message.guild.createdAt,true)
-	.addField(":satellite:**Sunucu Bölgesi**",message.guild.region,true)
+	.addField(":calendar:**Kuruluş Tarihi**",createserver,true)
+	.addField(":satellite:**Kanal Sayısı**",message.guild.channels.size,true)
+	.setFooter("**ID = **" + message.guild.id + " | " + "**Bölge = **" + message.guild.region)
 	message.channel.send(server)
 }
 
