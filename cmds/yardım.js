82df9c4231e15d089f9849f4bd2b91aed5dccf38
NexoTR <48154713+NexoTR@users.noreply.github.com>
2019-03-04 00:24:29 +0300

    Update yardım.js

--- cmds/yardım.js
+++ cmds/yardım.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 
 module.exports.run = async (bot, message, args) => {
 	if(!message.member.hasPermission("ADMINISTRATOR")){
-		var num = args[0];
+		var num = args.join(" ");
 		if (num.content === "eglence"){
 			message.channel.send("test")
 		}
